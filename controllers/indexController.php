33ba4fb90ac8d9ad4eeadc893661250af9cf21d8
gidzon <sumakaki37rus@gmail.com>
2019-06-06 14:35:39 +0300

    alteration commit

---
+++ controllers/indexController.php
@@ -0,0 +1,10 @@
+<?php
+namespace controllers;
+
+class indexController
+{
+    public function index()
+    {
+       echo 'hello';
+    }
+}
--- index.php
+++ index.php
@@ -6,17 +6,16 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\App;
 $container = $app->getContainer();
-$app->get('/hello[/{name}]', function ( $request,  $response) {
-    // $name = $request->getAttribute('id');
-    // $response->write( $name);
-    echo "string";
-    // return $response;
-
+$container['indexController'] = function ($container) {
+    
+    return $indexController;
+};
+$app->get('/', function($reg, $res, $args){
+    $indexController = $this->get('indexController');
+    return $res;
 });
-// $container['logger'] = function($c) {
-//     $logger = new \Monolog\Logger('my_logger');
-//     $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
-//     $logger->pushHandler($file_handler);
-//     return $logger;
-// };
+    
+
+
+
 $app->run();
